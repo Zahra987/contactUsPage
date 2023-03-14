aafee235f43936d5ec533e8837793fec820f6d29
zahra987 <zhr.ramezanpour@gmail.com>
2023-03-15 00:54:57 +0330

    navbar is working

--- script.js
+++ script.js
@@ -39,3 +39,14 @@ function upToDateFormValidation() {
   }
   return true;
 }
+
+var navLinks=document.getElementsByClassName('navLink');
+var nanLinkList=Object.values(navLinks);
+var link=document.getElementById('contactus');
+ nanLinkList.map((item) => {
+    if (item==link){
+     item.style.color="rgb(196, 196, 4)";
+    }else{
+       item.style.color="black";
+     }
+    })
